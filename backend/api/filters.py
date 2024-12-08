636c2107df288c30107b7b103bf2e2b6431c17f5
Руслан Брюханов <ruslanb@MacBook-Pro-Ruslan.local>
2024-12-08 13:28:56 +0500

    first commit

---
+++ backend/api/filters.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django_filters import FilterSet, filters
+from rest_framework.filters import SearchFilter
+
+from .models import Recipe, Tag
+
+User = get_user_model()
+
+
+class IngredientFilter(SearchFilter):
+    search_param = 'name'
+
+
+class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags')
+
+    def get_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(cart__user=self.request.user)
+        return queryset
