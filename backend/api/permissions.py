b3b39d10252cd078bb3ba1ecdf35df10460c1751
Руслан Брюханов <ruslanb@MacBook-Pro-Ruslan.local>
2025-03-31 10:16:30 +0500

    test

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class PublicOrAuthenticatedReadOnly(permissions.BasePermission):
+class AnonimOrAuthenticatedReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             (request.method in permissions.SAFE_METHODS
@@ -12,7 +12,7 @@ class PublicOrAuthenticatedReadOnly(permissions.BasePermission):
         )
 
 
-class CreatorOrReadOnly(permissions.BasePermission):
+class AuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
