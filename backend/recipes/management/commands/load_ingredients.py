5553917791008ea1a8b9884d0e46bbd1ddccc1e9
Руслан Брюханов <rametygames@gmail.com>
2025-05-22 22:54:28 +0500

    test

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,7 +1,6 @@
 import os
 import json
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from recipes.models import Ingredient  # замените на вашу модель
 
 
@@ -10,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = os.path.join(
-            settings.BASE_DIR, 'data', 'ingredients.json'
+            'data', 'ingredients.json'
         )
         with open(path, encoding='utf-8') as f:
             data = json.load(f)
