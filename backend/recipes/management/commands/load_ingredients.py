dcd1ed1d67a6f9d36972982019787ac859fbd33a
Руслан Брюханов <rametygames@gmail.com>
2025-05-22 22:38:53 +0500

    test

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,19 +1,20 @@
+import os
 import json
 from django.core.management.base import BaseCommand
-from recipes.models import Ingredient  # замените на своё
+from django.conf import settings
+from recipes.models import Ingredient  # замените на вашу модель
 
 
 class Command(BaseCommand):
     help = 'Load ingredients from JSON file'
 
     def handle(self, *args, **kwargs):
-        with open('backend/data/ingredients.json', encoding='utf-8') as f:
-            data = json.load(f)
-            for item in data:
-                Ingredient.objects.get_or_create(
-                    name=item['name'],
-                    measurement_unit=item['measurement_unit']
-                )
-        self.stdout.write(self.style.SUCCESS(
-            'Successfully loaded ingredients')
+        path = os.path.join(
+            settings.BASE_DIR, 'backend', 'data', 'ingredients.json'
         )
+        with open(path, encoding='utf-8') as f:
+            data = json.load(f)
+
+        for item in data:
+            Ingredient.objects.create(**item)
+            self.stdout.write(self.style.SUCCESS(f"Added {item}"))
